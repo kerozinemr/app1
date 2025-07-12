2ee520eb0147c905847be70e5340cdaee8a7513c
MazzX <155610819+kerozinemr@users.noreply.github.com>
2025-07-12 20:35:34 +0100

    config images s3- URL

--- main/models.py
+++ main/models.py
@@ -9,7 +9,7 @@ class Guest(models.Model):
     email = models.EmailField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    img = models.ImageField(null=True,storage=S3Boto3Storage(),default='main/static/images/profile_pic.jpg',upload_to='profile_images/')
+    img = models.ImageField(null=True,storage=S3Boto3Storage(),default='main/static/images/profile_pic.jpg',upload_to='static/images/')
     
     
     def __str__(self):
